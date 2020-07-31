1e634bdd31906684e8ffa22b60a678b663606c3a
Greg Zubatov <gzubatov@gmail.com>
2020-07-31 16:49:25 -0700

    Review form styling

--- src/components/Forms/ReviewForm.js
+++ src/components/Forms/ReviewForm.js
@@ -3,8 +3,6 @@ import { useHistory, useParams } from 'react-router-dom';
 import { useForm, Controller } from 'react-hook-form';
 import Select from 'react-select';
 
-import Input from '../FormElements/Input';
-
 const workloadOptions = [
 	{ value: 0, label: '0-5 hours' },
 	{ value: 1, label: '6-12 hours' },
@@ -57,10 +55,12 @@ const ReviewForm = (props) => {
 	return (
 		<div className="w-full">
 			<form
-				className="bg-white  rounded px-8 pt-6 pb-4 mb-4"
+				className="bg-white rounded shadow-lg px-8 pt-6 pb-4 mb-4 sm:w-full md:w-full lg:w-4/5 xl:w-4/5 m-auto"
 				onSubmit={handleSubmit(formHandler)}
 			>
-				<h2>Required</h2>
+				<h2 className="mb-4 border border-solid border-black border-t-0 border-l-0 border-r-0">
+					Required
+				</h2>
 				<div className="mb-4">
 					<label className="font-bold">Professor</label>
 					<Controller
@@ -68,16 +68,16 @@ const ReviewForm = (props) => {
 						name="Professor"
 						control={control}
 						options={props.professors}
-						className="w-1/6"
+						className="sm:w-full md:w-64 lg:w-64 xl:w-64"
 					/>
 				</div>
 				<div className="mb-4">
 					<h4 className="font-bold">How difficult is this course?</h4>
 					<div className="w-screen">
-						<p className="inline-block mr-12">
+						<p className="block sm:block md:inline-block lg:inline-block xl:inline-block  mr-12">
 							Anyone can get an A
 						</p>
-						<div className="inline-block mr-5">
+						<div className="block sm:block md:inline-block lg:inline-block xl:inline-block mr-5">
 							<input
 								className="mr-2"
 								type="radio"
@@ -86,7 +86,7 @@ const ReviewForm = (props) => {
 								ref={register({ required: true })}
 							/>1
 						</div>
-						<div className="inline-block mr-5">
+						<div className="block sm:block md:inline-block lg:inline-block xl:inline-block   mr-5">
 							<input
 								className="mr-2"
 								type="radio"
@@ -95,7 +95,7 @@ const ReviewForm = (props) => {
 								ref={register({ required: true })}
 							/>2
 						</div>
-						<div className="inline-block mr-5">
+						<div className="block sm:block md:inline-block lg:inline-block xl:inline-block   mr-5">
 							<input
 								className="mr-2"
 								type="radio"
@@ -104,7 +104,7 @@ const ReviewForm = (props) => {
 								ref={register({ required: true })}
 							/>3
 						</div>
-						<div className="inline-block mr-5">
+						<div className="block sm:block md:inline-block lg:inline-block xl:inline-block   mr-5">
 							<input
 								className="mr-2"
 								type="radio"
@@ -113,7 +113,7 @@ const ReviewForm = (props) => {
 								ref={register({ required: true })}
 							/>4
 						</div>
-						<div className="inline-block mr-12">
+						<div className="block sm:block md:inline-block lg:inline-block xl:inline-block   mr-12">
 							<input
 								className="mr-2"
 								type="radio"
@@ -122,7 +122,9 @@ const ReviewForm = (props) => {
 								ref={register({ required: true })}
 							/>5
 						</div>
-						<p className="inline-block">Prepare to get wrecked!</p>
+						<p className="block sm:block md:inline-block lg:inline-block xl:inline-block  ">
+							Prepare to get wrecked!
+						</p>
 					</div>
 					{errors.difficulty && <p>Required</p>}
 				</div>
@@ -140,7 +142,7 @@ const ReviewForm = (props) => {
 						control={control}
 						options={workloadOptions}
 						rules={{ required: true }}
-						className="w-1/6"
+						className="w-full sm:w-full md:w-1/4 lg:w-1/6 xl:w-1/6"
 					/>
 				</div>
 				<div className="mb-4">
@@ -158,7 +160,7 @@ const ReviewForm = (props) => {
 								control={control}
 								options={quarterOptions}
 								rules={{ required: true }}
-								className=""
+								className="w-full sm:w-full md:w-48 lg:w-48 xl:w-48"
 							/>
 						</div>
 						<div className="w-24 inline-block">
@@ -171,18 +173,28 @@ const ReviewForm = (props) => {
 								control={control}
 								options={yearOptions}
 								rules={{ required: true }}
-								className=""
+								className="w-full sm:w-full md:w-24 lg:w-24 xl:w-24"
 							/>
 						</div>
 					</div>
 				</div>
 
-				<h2>Optional</h2>
+				<h2 className="mb-4 border border-solid border-black border-t-0 border-l-0 border-r-0">
+					Optional{' '}
+					<span className="hidden sm:hidden md:inline-block lg:inline-block xl:inline-block">
+						(Answer as many or as few as you would like)
+					</span>
+				</h2>
 
-				<div className="w-2/5">
+				<div className="mb-4">
 					<label className="font-bold">
 						<h4 className="font-bold">Course Review</h4>
 					</label>
+					<p>
+						Tell your fellow Anteaters what you thought of the
+						course and any tips you have for how to do well in the
+						class.
+					</p>
 					<textarea
 						name="review"
 						ref={register}
@@ -191,7 +203,7 @@ const ReviewForm = (props) => {
 					/>
 				</div>
 
-				<div className="">
+				<div className="mb-4">
 					<label className="font-bold">
 						<h4 className="font-bold">
 							Do you recommend this course?
