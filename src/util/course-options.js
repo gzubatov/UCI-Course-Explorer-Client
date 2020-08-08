afebbffeea5925d468d0048daf5b57efa0d87a8f
Greg Zubatov <gzubatov@gmail.com>
2020-08-07 20:10:08 -0700

    Add more course departments

--- src/util/course-options.js
+++ src/util/course-options.js
@@ -1,8 +1,24 @@
 const courseSelectOptions = [
+	{ value: 'bio sci', label: 'BIO SCI' },
+	{ value: 'bme', label: 'BME' },
+	{ value: 'cbe', label: 'CBE' },
+	{ value: 'chem', label: 'CHEM' },
+	{ value: 'cogs', label: 'COGS' },
 	{ value: 'compsci', label: 'COMPSCI' },
+	{ value: 'cse', label: 'CSE' },
+	{ value: 'earthss', label: 'EARTHSS' },
+	{ value: 'econ', label: 'ECON' },
 	{ value: 'eecs', label: 'EECS' },
+	{ value: 'engrcee', label: 'ENGRCEE' },
+	{ value: 'engrmae', label: 'ENGRMAE' },
 	{ value: 'i&c sci', label: 'I&C SCI' },
 	{ value: 'in4matx', label: 'IN4MATX' },
+	{ value: 'math', label: 'MATH' },
+	{ value: 'mgmt', label: 'MGMT' },
+	{ value: 'mse', label: 'MSE' },
+	{ value: 'phrmsci', label: 'PHRMSCI' },
+	{ value: 'physics', label: 'PHYSICS' },
+	{ value: 'pol sci', label: 'POL SCI' },
 	{ value: 'stats', label: 'STATS' }
 ];
 
